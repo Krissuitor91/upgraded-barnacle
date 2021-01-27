858a6340fff61991583766b5391254836d4d3e28
Kristofer Suitor <krissuitor91@gmail.com>
2021-01-27 09:00:05 +0000

    Initialize and create index, about, readme and style file

---
+++ game_stats.py
@@ -0,0 +1,13 @@
+class GameStats:
+
+    def __init__(self, ai_game):
+        """Initialise stats."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        """Start game in an active state."""
+        self.game_active = True
+
+    def reset_stats(self):
+        """Stats that can change during the game."""
+        self.ships_left = self.settings.ship_limit
